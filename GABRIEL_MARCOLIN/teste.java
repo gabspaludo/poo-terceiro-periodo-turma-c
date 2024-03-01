850063cf19ec7b7fb16a3b50d5cf41b8bed5e60b
Gabriel Marcolin <gabrielmarcolin10@gmail.com>
2024-02-29 21:20:32 -0300

    Testeeeeeeeeee

---
+++ GABRIEL_MARCOLIN/teste.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class teste {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Qual é o seu nome?");
+        String nome = sc.nextLine();
+        System.out.println("Seu nome é " + nome);
+        System.out.println("Teste 1313123123");
+        
+    }
+}
