26b0d69f748f5d570726fdd214f3ad3f2c81975d
Vulpk <jotaviogs19705@gmail.com>
2024-03-06 20:14:13 -0300

    src

---
+++ GIULLIA_VILANOVA/src/main/PlantCalculator.java
@@ -0,0 +1,17 @@
+package main;
+
+import java.util.Scanner;
+
+public class PlantCalculator {
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int option;
+    
+
+        do {
+            exibitMenu();
+            option = scanner.nextInt();
+        }
+    }
+}
