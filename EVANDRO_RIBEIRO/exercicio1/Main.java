7531282ec9a63ac27e7abbb56eaddd3d29cc18ca
Evandrobr07 <evandrobrfilho@gmail.com>
2024-03-06 15:14:45 -0300

    Main.java

---
+++ EVANDRO_RIBEIRO/exercicio1/Main.java
@@ -0,0 +1,15 @@
+package exercicio1;
+
+public class Main {
+
+    static int teste = 20;
+    static int teste2 = 30;
+    static int teste3 = teste * teste2;
+
+    public static void main (String[]args){
+        System.out.println("Teste");
+
+        System.out.println(teste3);
+    }   
+
+}
