9adb578adf8294f4890103d84e8e2aaf14440b3b
Evandrobr07 <evandrobrfilho@gmail.com>
2024-03-07 02:04:54 -0300

    Lista 1

--- EVANDRO_RIBEIRO/exercicio1/Main.java
+++ EVANDRO_RIBEIRO/exercicio1/Main.java
@@ -1,15 +1,54 @@
 package exercicio1;
 
+import java.util.Scanner;
+
 public class Main {
-
-    static int teste = 20;
-    static int teste2 = 30;
-    static int teste3 = teste * teste2;
-
     public static void main (String[]args){
-        System.out.println("Teste");
+        Scanner scanner  = new Scanner(System.in);
+        System.out.println("[1] - Calcular Preço Total");
+        System.out.println("[2] - Calcular Troco");
+        System.out.println("[3] - Sair");
+            String escolha = scanner.nextLine();
 
-        System.out.println(teste3);
+        if (escolha.equals("1")) {
+            System.out.println("Qual planta foi vendida?");
+                String qualPlanta = scanner.nextLine();
+            
+            System.out.println("Quantas " + qualPlanta + "(s) foram vendidas?");
+                Float qntdPlanta = scanner.nextFloat();
+            
+            System.out.println("Qual é o valor da(o) " + qualPlanta + "? (Caso seja valor quebrado use 'n,n') ");
+                Float valorPlanta = scanner.nextFloat();
+
+            float valorFinal = (qntdPlanta * valorPlanta);
+
+            System.out.println("O valor da(o) " + qualPlanta + " é " + valorFinal + (" reais"));
+                scanner.nextLine();
+
+            System.out.println("[2] - Calcular Troco");
+            System.out.println("[3] - Sair");
+                String escolha2 = scanner.nextLine();
+
+            if (escolha2.equals("2")) {
+                System.out.println("Qual o valor de dinheiro recebido?");
+                    Float valorRece = scanner.nextFloat();
+                        
+                if(valorRece < valorFinal) {
+                    float trocoFalta = (valorRece - valorFinal) *-1;
+                    System.out.println("Ainda falta " + trocoFalta + " reais!");
+                }   
+                    else if(valorRece == valorFinal) {
+                        System.out.println("Não precisa de troco! ");
+                    } 
+                        else {
+                            float troco = (valorRece - valorFinal);
+                            System.out.println("O troco é de: " + troco + " reais!");
+                        }     
+            }
+                else if(escolha2.equals("3")) {
+                System.out.println("Obrigado por usar a calculadora!");
+                }   
+        }
+
     }   
-
 }
