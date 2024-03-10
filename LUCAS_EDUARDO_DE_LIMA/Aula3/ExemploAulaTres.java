ac7e1c8e1a2159fa09b177a1f5571314f2d77ee1
Lucaslimera <lucaslimacvel@hotmail.com>
2024-03-10 13:42:13 -0300

    exemploaulatres

---
+++ LUCAS_EDUARDO_DE_LIMA/Aula3/ExemploAulaTres.java
@@ -0,0 +1,14 @@
+package LUCAS_EDUARDO_DE_LIMA.Aula3;
+
+public class ExemploAulaTres {
+
+    public static void main (String[] args){
+       int idade = 20;
+       String valorPrint = idade >= 18 ? "É maior de idade" : "É menor de idade";
+
+       System.out.println(valorPrint);
+    }
+}
+
+
+
