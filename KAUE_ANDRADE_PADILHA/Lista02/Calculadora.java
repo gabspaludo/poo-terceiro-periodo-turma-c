e438d8a1a516fd39564d628b16e8618b75ab68da
Kaue Andrade <kapadilha@minha.fag.edu.br>
2024-03-12 21:53:52 -0300

    feat: melhorias calculadora

---
+++ KAUE_ANDRADE_PADILHA/Lista02/Calculadora.java
@@ -0,0 +1,162 @@
+import java.util.Scanner;
+
+public class Calculadora {  
+  private static int option;
+  private static double price; // Preço da planta
+  private static int quantity; // Quantidade comprada
+  private static double totalPrice; // Preço * Quantidade
+  private static double amountPaid; // Valor pago pelo cliente
+  private static double[][] sales = new double[100][3];
+  private static int salesCount = 0;
+  
+  @SuppressWarnings("resource")
+  public static void main(String[] args) {
+    
+    Scanner menu = new Scanner(System.in);
+    Scanner flowersQuantity = new Scanner(System.in);
+    Scanner pricePaid = new Scanner(System.in);
+
+    while (true) {
+      showMenu();
+
+      System.out.print("Digite uma opção: ");
+      option = menu.nextInt();
+
+      if (option == 0) {
+        System.out.println("Saindo do Menu...");
+        menu.close();
+        return;
+      }
+
+      switch (option) {
+        case 1:
+          price = 12.00;
+          
+          System.out.print("Quantas Rosas foram compradas? ");
+          quantity = flowersQuantity.nextInt();
+          totalPrice = calculateTotal(price, quantity); 
+          saveSell(sales, quantity, totalPrice);
+
+          System.out.print("Valor pago pelo cliente: ");
+          amountPaid = pricePaid.nextDouble();
+
+          calculateTroco(totalPrice, amountPaid);
+          break;
+        case 2:
+          price = 10.50;
+            
+          System.out.print("Quantos Girassóis foram comprados? ");
+          quantity = flowersQuantity.nextInt();
+          totalPrice = calculateTotal(price, quantity);
+          saveSell(sales, quantity, totalPrice);
+
+          System.out.print("Valor pago pelo cliente: ");
+          amountPaid = pricePaid.nextDouble();
+          calculateTroco(totalPrice, amountPaid);
+          break;
+        case 3:
+          price = 15.75;
+              
+          System.out.print("Quantas Orquídeas foram compradas? ");
+          quantity = flowersQuantity.nextInt();
+          totalPrice = calculateTotal(price, quantity);
+          saveSell(sales, quantity, totalPrice);
+
+          System.out.print("Valor pago pelo cliente: ");
+          amountPaid = pricePaid.nextDouble();
+          calculateTroco(totalPrice, amountPaid);
+          break;
+        case 4:
+          price = 14.00;
+
+          System.out.print("Quantos Lírios foram comprados? ");
+          quantity = flowersQuantity.nextInt();
+          totalPrice = calculateTotal(price, quantity);
+          saveSell(sales, quantity, totalPrice);
+
+          System.out.print("Valor pago pelo cliente: ");
+          amountPaid = pricePaid.nextDouble();
+          calculateTroco(totalPrice, amountPaid);
+          break;
+        case 5:
+          price = 8.50;
+              
+          System.out.print("Quantas Violetas foram compradas? ");
+          quantity = flowersQuantity.nextInt();
+          totalPrice = calculateTotal(price, quantity);
+          saveSell(sales, quantity, totalPrice);
+
+          System.out.print("Valor pago pelo cliente: ");
+          amountPaid = pricePaid.nextDouble();
+          calculateTroco(totalPrice, amountPaid);
+          break;
+        case 6:
+          showSells(sales, salesCount);
+          break;
+        default:
+          System.out.println("\nOpção Inválida!\n");
+          break;
+      }
+    }
+  }
+
+  public static void showMenu() {
+    System.out.println("|----------- LOJA DE PLANTAS -----------|");
+    System.out.println("|---------------------------------------|");
+    System.out.println("|            MENU PRINCIPAL             |");
+    System.out.println("|---------------------------------------|");
+    System.out.println("| 1 - Rosa:            R$ 12.00         |");
+    System.out.println("| 2 - Girassol:        R$ 10.50         |");
+    System.out.println("| 3 - Orquídea:        R$ 15.75         |");
+    System.out.println("| 4 - Lírio:           R$ 14.00         |");
+    System.out.println("| 5 - Violeta:         R$ 8.50          |");
+    System.out.println("|---------------------------------------|");
+    System.out.println("| 0 - Sair do Menu                      |");
+    System.out.println("| 6 - Ver Registro de Vendas            |");
+    System.out.println("|---------------------------------------|");
+  }
+
+  public static double calculateTotal(double price, int quantity) {
+    double totalPrice = price * quantity;
+
+    if (quantity > 10) {
+      double priceWithDiscount = totalPrice - (totalPrice * 0.05);
+      System.out.println("\nValor a ser pago: R$ " + priceWithDiscount);
+      return priceWithDiscount;
+    }
+
+    System.out.println("\nValor a ser pago: R$ " + totalPrice);
+    return totalPrice;
+  }
+
+  public static void calculateTroco(double totalPrice, double amountPaid) {
+    double amountReturned = amountPaid - totalPrice;
+    if (amountReturned > 0) {
+      System.out.println("Troco do cliente: R$ " + amountReturned + "\n");
+    } else if (amountReturned < 0) {
+      System.out.println("Faltam R$ " + (totalPrice - amountPaid) + "\n");
+    } else {
+      System.out.println("Não há troco a ser devolvido.\n");
+    }
+  }
+
+  public static void saveSell(double[][] sales, int quantity, double totalPrice) {
+    sales[salesCount][0] = quantity;
+    sales[salesCount][1] = totalPrice;
+    sales[salesCount][2] = totalPrice * 0.05;
+    salesCount++;
+  }
+
+  public static void showSells(double[][] sales, int salesCount) {
+    System.out.println("Registro de Vendas:");
+    for (int i = 0; i < salesCount; i++) {
+      System.out.println(
+        "Venda " + (i + 1) + 
+        ": Quantidade: " + sales[i][0] + 
+        ", Valor: R$ " + sales[i][1] + 
+        ", Desconto: R$" + sales[i][2]
+      );
+    }
+    System.out.println();
+  }
+}
