ce6479d1a3b129ee25094d1957b1f497a10b23a5
Luiz Colman <colmanluizz@gmail.com>
2024-03-16 02:09:53 -0300

    Exercício 2 finalizado

--- LUIZ_COLMAN/App.java
+++ LUIZ_COLMAN/App.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 
 public class App {
     static int user_action;
+    static double totalVendas = 0;
+    static double totalDescontos = 0;
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         in.useLocale(Locale.US);
@@ -11,37 +14,44 @@ public class App {
             System.out.println("------ESCOLHA UMA OPÇÃO------");
             System.out.println("[1] - Calcular Preço Total");
             System.out.println("[2] - Calcular Troco");
-            System.out.println("[3] - Sair");
+            System.out.println("[3] - Registrar Venda");
+            System.out.println("[4] - Sair");
 
             user_action = in.nextInt();
 
             switch (user_action) {
                 case 1:
                     System.out.println("Insira a quantidade de plantas:");
-                    String quantPlantas = in.next();
+                    double quantPlantas = in.nextDouble();
                     System.out.println("Insira o valor das plantas:");
-                    String valorPlantas = in.next();
+                    double valorPlantas = in.nextDouble();
                     System.out.println("A quantidade de plantas é: "+ quantPlantas + "\nO valor de cada planta é: " + "R$" + valorPlantas);
-                    int calcPlantas;
-                    calcPlantas = (int) Integer.parseInt(quantPlantas) * Integer.parseInt(valorPlantas);
-                    System.out.println("O preço total da compra das plantas é: " + "R$" + calcPlantas);
+                    
+                    double calcPlantas;
+                    double calcPlantasDesconto;
+                    calcPlantas = quantPlantas * valorPlantas;
+                    calcPlantasDesconto = (quantPlantas > 10) ? calcPlantas * 0.95 : calcPlantas;
+                    totalVendas += calcPlantasDesconto;
+                    System.out.println("O preço total da compra das plantas é: " + "R$" + calcPlantasDesconto);
                     break;
                 case 2:
                     System.out.println("Insira o valor pago pelo cliente:");
-                    String valorPago = in.next();
+                    double valorPago = in.nextDouble();
                     System.out.println("Insira o valor total da compra:");
-                    String valorTotal = in.next();
+                    double valorTotal = in.nextDouble();
                     double valorTroco;
-                    valorTroco = (double) Integer.parseInt(valorPago) - Integer.parseInt(valorTotal);
+                    valorTroco = valorPago - valorTotal;
                     System.out.println("O troco é: R$" + valorTroco);
                     break;
                 case 3:
+                    System.out.println("Venda registrada.");
+                    break;
+                case 4:
                     break;
                 default:
                     break;
             }
-            } while (user_action != 4);
-            in.close();
+        } while (user_action != 4);
+        in.close();
     }
-    
 }
