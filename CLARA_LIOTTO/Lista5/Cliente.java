301987a6d65be620e65976e3e20bb3cdc986fdbe
mclliotto <mclliotto@minha.fag.edu.br>
2024-04-11 22:21:30 -0300

    lista5

---
+++ CLARA_LIOTTO/Lista5/Cliente.java
@@ -0,0 +1,13 @@
+package CLARA_LIOTTO.Lista5;
+
+public class Cliente {
+    public String nome = "Eduardo";
+    public int idade = 25;
+    public String cidade = "Rio de Janeiro";
+    public String bairro = "Copacabana";
+    public String rua = "Avenida Atlântica";
+    
+    public void apresentarSe() {
+            System.out.println("Nome: " + nome + ", Idade: " + idade);
+        }
+    }
