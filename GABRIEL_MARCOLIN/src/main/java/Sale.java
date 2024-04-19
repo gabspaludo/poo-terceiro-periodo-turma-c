52926bd8015fcdcf249344e84975c0162bab25ae
Gabriel Marcolin de Carvalho <146651956+Marcoliiin@users.noreply.github.com>
2024-04-19 20:36:50 -0300

    Update Sale.java

--- GABRIEL_MARCOLIN/src/main/java/Sale.java
+++ GABRIEL_MARCOLIN/src/main/java/Sale.java
@@ -1,68 +1,107 @@
-import java.util.Scanner;
+import java.util.Date;
 
-public class Sale {
-    double valor;
-    int quantity;
-    double payment;
-    double total;
-    double change;
-    double discount;
-    static Sale[] sales;
-    int saleQuantity;
-    Scanner sc = new Scanner(System.in);
+public class Pedido {
 
-    public Sale() {
-        System.out.println("Qual o valor do produto (em reais) que o cliente está comprando?");
-        this.valor = sc.nextDouble();
-        System.out.println("Qual a quantidade de produtos que ele está comprando?");
-        this.quantity = sc.nextInt();
-        System.out.println("Quantos reais o cliente está fornecendo para pagar a compra?");
-        this.payment = sc.nextDouble();
+    private int id;
 
-        calculatingChange();
-        sales = new Sale[saleQuantity];
+    private Date dataCriacao;
+
+    private Date dataPagamento;
+
+    private Date dataVencimentoReserva;
+
+    private Cliente cliente;
+
+    private Vendedor vendedor;
+
+    private Loja loja;
+
+    private Item[] itens;
+
+    public Pedido() {
+        Long umDia = 86400000l;
+        Long criacaoPedido = new Date().getTime();
+        Long vencimentoReserva = criacaoPedido + (umDia * 3);
+
+        this.dataCriacao = new Date();
+        this.dataVencimentoReserva = new Date(vencimentoReserva);
     }
 
-    //Esta função foi utilizada para realizar o cadastro do pedido, multiplicando o valor do produto pela quantidade do mesmo.
+    public int getId() {
+        return id;
+    }
 
-    public double saleTotal() {
+    public void setId(int id) {
+        this.id = id;
+    }
 
-        if (this.quantity < 11) {
-            this.total = (this.valor * this.quantity);
-            System.out.println("O total da compra do cliente ficou: " + this.total + " reais.");
-        } else {
-            System.out.println("Desconto de 5% aplicado ao valor total de: " + String.valueOf(this.total = (this.valor * this.quantity)) + " reais.");
-            System.out.println("O desconto é de: " + String.valueOf(this.discount = (this.total * 0.05)) + " reais.");
+    public Date getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(Date dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public Loja getLoja() {
+        return loja;
+    }
+
+    public Item[] getItens() {
+        return itens;
+    }
+
+    public void setItens(Item[] itens) {
+        this.itens = itens;
+    }
+
+    public void setLoja(Loja loja) {
+        this.loja = loja;
+    }
+
+    public Vendedor getVendedor() {
+        return vendedor;
+    }
+
+    public void setVendedor(Vendedor vendedor) {
+        this.vendedor = vendedor;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Date getDataVencimentoReserva() {
+        return dataVencimentoReserva;
+    }
+
+    public void setDataVencimentoReserva(Date dataVencimentoReserva) {
+        this.dataVencimentoReserva = dataVencimentoReserva;
+    }
+
+    public Date getDataPagamento() {
+        return dataPagamento;
+    }
+
+    public void setDataPagamento(Date dataPagamento) {
+        this.dataPagamento = dataPagamento;
+    }
+
+    public void calcularValorTotal() {
+        double valorTotal = 0;
+
+        for (int i = 0; i < itens.length; i++) {
+            valorTotal += itens[i].getValor();
         }
-
-        double saleTotal = (this.total - this.discount);
-        return saleTotal;
+        System.out.println("O valor total da venda é: " + valorTotal);
     }
 
-    //Esta função foi utilizada para calcular o troco do cliente APENAS se o cliente já tiver um pedido criado,
-    //no caso, se ele tiver selecionado a opção 1.
-
-    public void calculatingChange() {
-
-        this.change = (saleTotal() - this.payment);
-
-        //Este if foi utilizado para validar se o cliente está fornecendo o dinheiro necessário para zerar
-        //o valor que precisa ser pago. Vale ressaltar que foi utilizado a função Math.abs para tornar o
-        //valor de troco positivo, já que é o valor que o cliente ainda vai precisar pagar.
-
-        if (this.change > 0) {
-            System.out.println("O cliente ainda precisa pagar " + this.change + " reais.");
-        } else {
-            System.out.println("O troco que ficou para o cliente é de " + Math.abs(this.change) + " reais.");
-        }
-    }
-
-    //Função que aumenta o array original com base num novo array, que tem os mesmos
-    //valores que o array original, porém, com +1 de tamanho.
-
-    public static Sale[] increaseArraySize(Sale[] array) {
-        Sale[] newArray = new Sale[array.length + 1];
-        System.arraycopy(array, 0, newArray, 0, array.length);
-        return newArray;
+    public void gerarDescricaoVenda() {
+        calcularValorTotal();
+        System.out.println("A data de criação do seu pedido é: " + getDataCriacao());
     }
 }
