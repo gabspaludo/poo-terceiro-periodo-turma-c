782cb2ad2b615bf043297cfe2ee52780e2df9c0d
Kaue-Dev <kauemassola900@gmail.com>
2024-04-24 19:47:16 -0300

    lista 7

--- KAUE_ANDRADE_PADILHA/lista07/Loja.java
+++ KAUE_ANDRADE_PADILHA/lista07/Loja.java
@@ -1,56 +1,90 @@
 package lista07;
 
 public class Loja {
-  public String nome;
-  public Endereco endereco;
-  public String razaoSocial;
-  private String cnpj;
-  public Vendedor[] vendedores = new Vendedor[3];
-  public Cliente[] clientes = new Cliente[3];
+    public String nome;
+    public Endereco endereco;
+    public String razaoSocial;
+    private String cnpj;
+    private Vendedor[] vendedores = new Vendedor[3];
+    private Cliente[] clientes = new Cliente[3];
+    private Item[] itens = new Item[100];
+    private Integer posicaoItem = 0;
 
-  public Loja(String nome, String razaoSocial, Endereco endereco, Vendedor[] vendedores, Cliente[] clientes) {
-    this.nome = nome;
-    this.razaoSocial = razaoSocial;
-    this.endereco = endereco;
-    this.vendedores = vendedores;
-    this.clientes = clientes;
-  }
-
-  public void contarVendedores() {
-    int cont = 0;
-
-    for (int i = 0; i < vendedores.length; i++) {
-      if (vendedores[i] != null) {
-        cont++;
-      }
+    public Loja(String nome, String razaoSocial, Endereco endereco, Vendedor[] vendedores, Cliente[] clientes) {
+        this.nome = nome;
+        this.razaoSocial = razaoSocial;
+        this.endereco = endereco;
+        this.vendedores = vendedores;
+        this.clientes = clientes;
     }
 
-    System.out.println("Número de vendedores " + nome + ": " + cont);
-  }
+    public void contarVendedores() {
+        int cont = 0;
 
-  public void contarClientes() {
-    int cont = 0;
+        for (int i = 0; i < vendedores.length; i++) {
+            if (vendedores[i] != null) {
+                cont++;
+            }
+        }
 
-    for (int i = 0; i < clientes.length; i++) {
-      if (clientes[i] != null) {
-        cont++;
-      }
+        System.out.println("Número de vendedores " + nome + ": " + cont);
     }
 
-    System.out.println("Número de clientes: " + nome + ": " + cont);
-  }
+    public void contarClientes() {
+        int cont = 0;
 
-  public void apresentarse() {
-    String msg = "Olá, somos a loja " + nome + ", Razão Social: " + razaoSocial + ", CNPJ: " + cnpj
-        + ", Localizada na cidade: " + endereco.getCidade() + ", Bairro: " + endereco.getBairro() + ", Rua: " + endereco.getRua();
-    System.out.println(msg);
-  }
+        for (int i = 0; i < clientes.length; i++) {
+            if (clientes[i] != null) {
+                cont++;
+            }
+        }
 
-  public String getCnpj() {
-    return cnpj;
-  }
+        System.out.println("Número de clientes: " + nome + ": " + cont);
+    }
 
-  public void setCnpj(String cnpj) {
-    this.cnpj = cnpj;
-  }
+    public void apresentarse() {
+        String msg = "Olá, somos a loja " + nome + ", Razão Social: " + razaoSocial + ", CNPJ: " + cnpj
+                + ", Localizada na cidade: " + endereco.getCidade() + ", Bairro: " + endereco.getBairro() + ", Rua: " + endereco.getRua();
+        System.out.println(msg);
+    }
+
+    public void cadastrarItem(Item item) {
+        item.setId(posicaoItem);
+        itens[posicaoItem++] = item;
+    }
+
+    public void listarItens() {
+        System.out.println("Itens: ");
+        for (int i = 0; i < posicaoItem; i++) {
+            itens[i].gerarDescricao();
+        }
+    }
+
+    public Item selecionarItem(int id) {
+        return itens[id];
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public Vendedor[] getVendedores() {
+        return vendedores;
+    }
+
+    public Cliente[] getClientes() {
+        return clientes;
+    }
+
+    public Item[] getItens() {
+        return itens;
+    }
+
+    public Integer size() {
+        return posicaoItem;
+    }
 }
--- KAUE_ANDRADE_PADILHA/lista07/Pedido.java
+++ KAUE_ANDRADE_PADILHA/lista07/Pedido.java
@@ -3,113 +3,113 @@ package lista07;
 import java.util.Date;
 
 public class Pedido {
-  private Integer id;
-  private Date dataCriacao;
-  private Date dataPagamento;
-  private Date dataVencimento;
-  private Cliente cliente;
-  private Vendedor vendedor;
-  private Loja loja;
-  private String empresaParceira;
-  public Item[] itens = new Item[10];
+    private Integer id;
+    private Date dataCriacao;
+    private Date dataPagamento;
+    private Date dataVencimento;
+    private Cliente cliente;
+    private Vendedor vendedor;
+    private Loja loja;
+    private String empresaParceira;
+    public Item[] itens = new Item[10];
 
-  private int posicao = 0;
+    private int posicao = 0;
 
-  public void salvar(Item item) {
-    itens[posicao++] = item;
-    item.id = posicao;
-  }
-
-  public float calcularValorTotal() {
-    if (itens[0] == null) return 0;
-
-    Float valorTotal = 0.0f;
-
-    for (int i = 0; i < itens.length; i++) {
-      valorTotal += itens[i].valor;
+    public void adicionarItem(Item item) {
+        itens[posicao++] = item;
     }
 
-    System.out.println("Valor total: " + valorTotal);
-    return valorTotal;
-  }
+    public float calcularValorTotal() {
+        if (itens[0] == null) return 0;
 
-  public void gerarDescricaoVenda() {
-    String msg = "Data de criação: ".concat(dataCriacao.toString())
-    //.concat("\nItens: ").concat(gerarDescricaoItens()) // Exibir isso
-    .concat("\nValor total: ").concat(calcularValorTotal() + "")
-    .concat("\nEm parceria com: ").concat(empresaParceira);
-    System.out.println(msg);
-  }
+        Float valorTotal = 0.0f;
 
-  public void gerarDescricaoItens() {
-    for (int i = 0; i < itens.length; i++) {
-      if (itens[i] != null) {
-        itens[i].gerarDescricao();
-      }
+        for (int i = 0; i < posicao; i++) {
+            valorTotal += itens[i].getValor();
+        }
+
+        System.out.println("Valor total: " + valorTotal);
+        return valorTotal;
     }
-  }
 
-  public void setId(Integer id) {
-    this.id = id;
-  }
+    public void gerarDescricaoVenda() {
+        System.out.println("\nData de criação: ".concat(dataCriacao.toString())
+        .concat("\nItens: "));
+        gerarDescricaoItens();
+        System.out.println("Valor total: ".concat(calcularValorTotal() + "")
+        .concat("\nEm parceria com: ").concat(empresaParceira));
+    }
 
-  public Integer getId() {
-    return id;
-  }
 
-  public void setDataCriacao(Date dataCriacao) {
-    this.dataCriacao = dataCriacao;
-  }
+    public void gerarDescricaoItens() {
+        for (int i = 0; i < itens.length; i++) {
+            if (itens[i] != null) {
+                itens[i].gerarDescricao();
+            }
+        }
+    }
 
-  public Date getDataCriacao() {
-    return dataCriacao;
-  }
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-  public void setDataPagamento(Date dataPagamento) {
-    this.dataPagamento = dataPagamento;
-  }
+    public Integer getId() {
+        return id;
+    }
 
-  public Date getDataPagamento() {
-    return dataPagamento;
-  }
+    public void setDataCriacao(Date dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
 
-  public void setDataVencimento(Date dataVencimento) {
-    this.dataVencimento = dataVencimento;
-  }
+    public Date getDataCriacao() {
+        return dataCriacao;
+    }
 
-  public Date getDataVencimento() {
-    return dataVencimento;
-  }
+    public void setDataPagamento(Date dataPagamento) {
+        this.dataPagamento = dataPagamento;
+    }
 
-  public void setCliente(Cliente cliente) {
-    this.cliente = cliente;
-  }
+    public Date getDataPagamento() {
+        return dataPagamento;
+    }
 
-  public Cliente getCliente() {
-    return cliente;
-  }
+    public void setDataVencimento(Date dataVencimento) {
+        this.dataVencimento = dataVencimento;
+    }
 
-  public void setVendedor(Vendedor vendedor) {
-    this.vendedor = vendedor;
-  }
+    public Date getDataVencimento() {
+        return dataVencimento;
+    }
 
-  public Vendedor getVendedor() {
-    return vendedor;
-  }
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
 
-  public void setLoja(Loja loja) {
-    this.loja = loja;
-  }
+    public Cliente getCliente() {
+        return cliente;
+    }
 
-  public Loja getLoja() {
-    return loja;
-  }
+    public void setVendedor(Vendedor vendedor) {
+        this.vendedor = vendedor;
+    }
 
-  public void setEmpresaParceira(String empresaParceira) {
-    this.empresaParceira = empresaParceira;
-  }
+    public Vendedor getVendedor() {
+        return vendedor;
+    }
 
-  public String getEmpresaParceira() {
-    return empresaParceira;
-  }
+    public void setLoja(Loja loja) {
+        this.loja = loja;
+    }
+
+    public Loja getLoja() {
+        return loja;
+    }
+
+    public void setEmpresaParceira(String empresaParceira) {
+        this.empresaParceira = empresaParceira;
+    }
+
+    public String getEmpresaParceira() {
+        return empresaParceira;
+    }
 }
